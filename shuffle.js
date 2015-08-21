a2463e399fe5d85ae4b0686d47be1c2c5fe4f472
reydelo <reyna.deloge@gmail.com>
2015-08-21 12:34:57 -0600

    create shuffleChunk function

--- shuffle.js
+++ shuffle.js
@@ -1,1 +1,31 @@
-console.log('test');
+function shuffle(array) {
+  var counter = array.length, temp, index;
+  while (counter > 0) {
+      index = Math.floor(Math.random() * counter);
+      counter--;
+      temp = array[counter];
+      array[counter] = array[index];
+      array[index] = temp;
+  }
+  return array;
+}
+
+function chunk(shuffledArray, size) {
+  var chunks = [],
+      i = 0,
+      n = shuffledArray.length,
+      num = n/size;
+  while (i < n) {
+    chunks.push(shuffledArray.slice(i, i += num));
+  }
+  return chunks;
+}
+
+function shuffleChunk(inputArray, num) {
+  var shuffledArray = shuffle(inputArray);
+  return chunk(shuffledArray, num);
+}
+
+var inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+console.log(shuffleChunk(inputArray, 4));
